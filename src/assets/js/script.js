da7ffd23ac3c78901afe4977db085f882a285a4d
DESKTOP-Infobase\Pedro <pedro.borges@infobase.com.br>
2019-02-07 11:43:32 -0500

    third commit-11:45am

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,8 +3,21 @@ $(document).ready(function () {
    //alert("test");
     alert(sum(7,6));
     console.log(sum(7,6));
+
+    arrays();
 });
 
 function sum(a, b) {
     return a + b;
+}
+
+function arrays() {
+    var pens;
+    pens = ['green',"blue", 'red', "gray", "black"];
+    console.log(pens);
+    pens.push("white");
+    console.log(pens);
+    pens.push("#056598");
+    console.log(pens);
+
 }
